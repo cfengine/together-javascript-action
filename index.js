b1b39b29bfdc1f6ba260bae208016f722d099d6a
Craig Comstock <craig.comstock@northern.tech>
2023-04-12 16:05:52 -0500

    First batch of fixes

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-console.log(context);
-console.log(github);
-          body = "";
-          prs = {};
-          if (context.issue.body) {
-            // Return issue body if present
-            body = context.issue.body;
-          } else {
-            // Otherwise return issue body from commit
-            pull_requests = await github.rest.repos.listPullRequestsAssociatedWithCommit({
-                commit_sha: context.sha,
-                owner: context.repo.owner,
-                repo: context.repo.repo,
-              });
-            body = pull_requests.data[0].body;
-          }
+const core = require('@actions/core');
+const github = require('@actions/github');
+const context = github.context;
+
+async function run() {
+const myToken = core.getInput('myToken');
+// use myToken: ${{ secrets.GITHUB_TOKEN }} in your workflow
+const octokit = github.getOctokit(myToken);
+//console.log(github);
+req = {
+  owner: context.payload.repository.owner.login,
+  repo: context.payload.repository.name,
+  pull_number: context.payload.pull_request.number
+};
+//console.log("req is ", req);
+const { data: pullRequest } = await octokit.rest.pulls.get(
+  req
+);
+//console.log("pullRequest is ", pullRequest);
+
+body = pullRequest.body;
+          refs = {};
+//          console.log("found body=",body);
           const regexp = /https:\/\/github.com\/cfengine\/([a-z]*)\/pull\/(\d*)/g;
           const matches = body.matchAll(regexp);
           for (const match of matches) {
-            prs[match[1]] = match[2];
+core.setOutput(match[1], "refs/pull/" + match[2] + "/head");
           }
-console.log(prs);
+}
+
+run();
