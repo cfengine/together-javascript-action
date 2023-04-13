0e634c3864255a6fe950431ec51e33b72415447f
Craig Comstock <craig.comstock@northern.tech>
2023-04-13 16:18:34 -0500

    Second batch of initial fixes, ready to go

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const context = github.context;
+const core = require('@actions/core')
+const github = require('@actions/github')
+const context = github.context
 
-async function run() {
-const myToken = core.getInput('myToken');
-// use myToken: ${{ secrets.GITHUB_TOKEN }} in your workflow
-const octokit = github.getOctokit(myToken);
-//console.log(github);
-req = {
-  owner: context.payload.repository.owner.login,
-  repo: context.payload.repository.name,
-  pull_number: context.payload.pull_request.number
-};
-//console.log("req is ", req);
-const { data: pullRequest } = await octokit.rest.pulls.get(
-  req
-);
-//console.log("pullRequest is ", pullRequest);
+async function run () {
+  const myToken = core.getInput('myToken')
+  // use myToken: ${{ secrets.GITHUB_TOKEN }} in your workflow
+  const octokit = github.getOctokit(myToken)
+  // console.log(github);
+  const req = {
+    owner: context.payload.repository.owner.login,
+    repo: context.payload.repository.name,
+    pull_number: context.payload.pull_request.number
+  }
+  // console.log("req is ", req);
+  const { data: pullRequest } = await octokit.rest.pulls.get(
+    req
+  )
+  // console.log("pullRequest is ", pullRequest);
 
-body = pullRequest.body;
-          refs = {};
-//          console.log("found body=",body);
-          const regexp = /https:\/\/github.com\/cfengine\/([a-z]*)\/pull\/(\d*)/g;
-          const matches = body.matchAll(regexp);
-          for (const match of matches) {
-core.setOutput(match[1], "refs/pull/" + match[2] + "/head");
-          }
+  const body = pullRequest.body
+  //          console.log("found body=",body);
+  const regexp = /https:\/\/github.com\/cfengine\/([a-z]*)\/pull\/(\d*)/g
+  const matches = body.matchAll(regexp)
+  for (const match of matches) {
+    core.setOutput(match[1], 'refs/pull/' + match[2] + '/head')
+  }
 }
 
-run();
+run()
