c418e2b63bc9d5f38dccd952b7ba18f26529ed5b
Craig Comstock <craig.comstock@northern.tech>
2025-04-15 15:06:11 -0500

    test core.info logging

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ async function run () {
   const myToken = core.getInput('myToken')
   // use myToken: ${{ secrets.GITHUB_TOKEN }} in your workflow
   const octokit = github.getOctokit(myToken)
-  console.log("github object is ", github);
+  core.info("github object is ", github);
   const req = {
     owner: context.payload.repository.owner.login,
     repo: context.payload.repository.name,
     pull_number: context.payload.pull_request.number
   }
-  // console.log("req is ", req);
+  core.info("req is ", req);
   const { data: pullRequest } = await octokit.rest.pulls.get(
     req
   )
-  console.log("pullRequest is ", pullRequest);
+  core.info("pullRequest is ", pullRequest);
 
   const body = pullRequest.body
-  //          console.log("found body=",body);
+           core.info("found body=",body);
   const regexp = /https:\/\/github.com\/cfengine\/([A-Za-z0-9_.-]*)\/pull\/(\d*)/g
   let matches = []
   if (body) {
@@ -31,10 +31,10 @@ async function run () {
   }
   for (const match of matches) {
     core.setOutput(match[1], 'refs/pull/' + match[2] + '/merge')
-    console.log(`added ${match[1]} => refs/pull/${match[2]}/merge`);
+    core.info(`added ${match[1]} => refs/pull/${match[2]}/merge`);
   }
   core.setOutput(context.payload.repository.name, context.ref);
-  console.log(`added pull request ref ${context.payload.repository.name} => ${context.ref}`);
+  core.info(`added pull request ref ${context.payload.repository.name} => ${context.ref}`);
 }
 
 run()
